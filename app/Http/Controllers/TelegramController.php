c116b840b7b302f4c795adc2b83e69e165eb8771
shahram peyvandi <yasfuny@gmail.com>
2019-07-29 22:04:11 -0700

    s

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -7,10 +7,10 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
-    public function telegram()
-    {
-        ini_set('max_execution_time', 30);
-
-        return Telegram::getMe();
-    }
+//    public function telegram()
+//    {
+//        ini_set('max_execution_time', 30);
+//
+//        return Telegram::getMe();
+//    }
 }
